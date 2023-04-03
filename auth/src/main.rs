96821fb08575213998ce6065f30fec9a2db79192
montaglue <ev.pak.moc.liamg@gmail.com>
2023-04-03 17:25:50 +0300

    cargo fix

--- auth/src/main.rs
+++ auth/src/main.rs
@@ -1,9 +1,6 @@
-use axum::{
-    routing::{get, post},
-    Router,
-};
-use tokio::net::unix::SocketAddr;
-use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
+
+
+
 
 #[tokio::main]
 async fn main() {
